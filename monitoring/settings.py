4a9ffe0f50c046eda996f3d4d9404ca698122234
Daniela Parra <d.parram2@uniandes.edu.co>
2023-02-13 00:00:22 -0500

    Cambios nuevas funcionalidades de measurement

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'monitoring.wsgi.application'
 DATABASES = {
      "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "<nombrebd>",
-        "USER": "<usuario>",
-        "PASSWORD": "<clave>",
+        "NAME": "baseDatos",
+        "USER": "danielaParra",
+        "PASSWORD": "Simon2020",
         "HOST": "localhost",
-        "PORT": "",
+        "PORT": "5432",
     }
 }
 
